5e37490c22f9dece6df4a180c5f3e88324d52446
Michael Russo <mjr@mjrusso.com>
2024-03-16 14:34:57 -0400

    Don't configure the MacOS dock.

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -11,9 +11,6 @@ let
   additionalFiles = import ./files.nix { inherit user config pkgs; };
 in
 {
-  imports = [
-   ./dock
-  ];
 
   # It me
   users.users.${user} = {
@@ -62,33 +59,5 @@ in
     };
   };
 
-  # Fully declarative dock using the latest from Nix Store
-  local.dock.enable = true;
-  local.dock.entries = [
-    { path = "/Applications/Slack.app/"; }
-    { path = "/System/Applications/Messages.app/"; }
-    { path = "/System/Applications/Facetime.app/"; }
-    { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
-    { path = "/System/Applications/Music.app/"; }
-    { path = "/System/Applications/News.app/"; }
-    { path = "/System/Applications/Photos.app/"; }
-    { path = "/System/Applications/Photo Booth.app/"; }
-    { path = "/System/Applications/TV.app/"; }
-    { path = "/System/Applications/Home.app/"; }
-    {
-      path = toString myEmacsLauncher;
-      section = "others";
-    }
-    {
-      path = "${config.users.users.${user}.home}/.local/share/";
-      section = "others";
-      options = "--sort name --view grid --display folder";
-    }
-    {
-      path = "${config.users.users.${user}.home}/.local/share/downloads";
-      section = "others";
-      options = "--sort name --view grid --display stack";
-    }
-  ];
 
 }
--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -3,5 +3,4 @@
 with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
-  dockutil
 ]
