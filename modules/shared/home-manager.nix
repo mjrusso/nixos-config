b31ffe322dfc7757a55709e57246acfb2ca278f4
Michael Russo <mjr@mjrusso.com>
2024-03-16 14:45:22 -0400

    Configure git aliases.

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -184,6 +184,31 @@ let name = "%NAME%";
     lfs = {
       enable = true;
     };
+    aliases = {
+      d   = "diff";
+      dc  = "diff --cached";
+      f   = "fetch";
+      fo  = "fetch origin";
+      co  = "checkout";
+      com = "checkout main";
+      pom = "pull origin main";
+      cod = "checkout develop";
+      pod = "pull origin develop";
+      st  = "status -sb";
+      su  = "submodule update";
+      g   = "grep --break --heading --line-number";
+      up  = "!git pull --rebase --prune $@ && git submodule update --init --recursive";
+      save  = "!git add -A && git commit -m 'SAVEPOINT'";
+      wip   = "!git add -u && git commit -m "WIP"";
+      undo  = "reset HEAD~1 --mixed";
+      amend = "commit -a --amend";
+      shortsha = "rev-parse --short HEAD";
+      lg  = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      lgp = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative -p";
+      wc = "whatchanged --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      refs = "for-each-ref --sort='authordate:iso8601' --format=' %(color:green)%(authordate:relative)%09%(color:white)%(refname:short)' refs/heads";
+      count = "!git shortlog -sn";
+    };
     extraConfig = {
       init.defaultBranch = "main";
       core = { 
