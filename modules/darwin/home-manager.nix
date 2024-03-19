1fb428d83b0f3f1c3581f1eb0e508e017babb1c2
Michael Russo <mjr@mjrusso.com>
2024-03-19 14:22:58 -0400

    Re-enable manpages.

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -29,10 +29,6 @@ in
         stateVersion = "23.11";
       };
       programs = {} // import ../shared/home-manager.nix { inherit config osConfig pkgs lib; };
-
-      # Marked broken Oct 20, 2022 check later to remove this
-      # https://github.com/nix-community/home-manager/issues/3344
-      manual.manpages.enable = false;
     };
   };
 
