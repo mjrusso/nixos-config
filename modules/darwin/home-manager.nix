b1517faadc6fb86d87a98cf376d500553d7474ba
Michael Russo <mjr@mjrusso.com>
2024-10-24 22:20:41 -0400

    Format with `nixfmt`.

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -4,8 +4,7 @@ let
   user = "mjrusso";
   sharedFiles = import ../shared/files.nix { inherit config pkgs; };
   additionalFiles = import ./files.nix { inherit user config pkgs; };
-in
-{
+in {
 
   users.users.${user} = {
     name = "${user}";
@@ -17,7 +16,7 @@ in
   # Enable home-manager
   home-manager = {
     useGlobalPkgs = true;
-    users.${user} = { pkgs, config, osConfig, lib, ... }:{
+    users.${user} = { pkgs, config, osConfig, lib, ... }: {
       imports = [
         # Use the `mac-app-util` module to ensure that app launchers (e.g.
         # Emacs) are properly symlinked (so they can be found via Spotlight,
@@ -30,18 +29,17 @@ in
       ];
       home = {
         enableNixpkgsReleaseCheck = false;
-        packages = pkgs.callPackage ./packages.nix {};
-        sessionPath = [];
+        packages = pkgs.callPackage ./packages.nix { };
+        sessionPath = [ ];
         sessionVariables = {
           EDITOR = "${pkgs.my-emacs-with-packages}/bin/emacsclient";
         };
-        file = lib.mkMerge [
-          sharedFiles
-          additionalFiles
-        ];
+        file = lib.mkMerge [ sharedFiles additionalFiles ];
         stateVersion = "23.11";
       };
-      programs = {} // import ../shared/home-manager.nix { inherit config osConfig pkgs lib; };
+      programs = { } // import ../shared/home-manager.nix {
+        inherit config osConfig pkgs lib;
+      };
     };
   };
 
