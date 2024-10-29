50646da4bc09204e9d81490690571c5b7c6ab701
Michael Russo <mjr@mjrusso.com>
2024-10-28 21:33:49 -0400

    Configure direnv to write cache files to `~/.cache/direnv/layouts/`.

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -10,6 +10,21 @@ let name = "Michael Russo";
   direnv = {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = ''
+      # Write cache files to `~/.cache/direnv/layouts/`, instead of creating a
+      # `.direnv/` directory directly inside each direnv-enabled directory. See:
+      # https://github.com/direnv/direnv/wiki/Customizing-cache-location
+      : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+          local hash path
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+              hash="$(sha1sum - <<< "$PWD" | head -c40)"
+              path="''${PWD//[^a-zA-Z0-9]/-}"
+              echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+          )}"
+      }
+    '';
   };
 
   fish = {
