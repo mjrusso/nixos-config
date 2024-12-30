58d676b1c901a8f1641d165df691e44fa24c64b2
Michael Russo <mjr@mjrusso.com>
2024-12-30 14:23:19 -0500

    Install `nix-env.fish` plugin on non-NixOS Linux systems.
    This fixes the $PATH being incorrect after reboot, among other issues. See
    https://github.com/lilyball/nix-env.fish for more details.

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -30,6 +30,27 @@ let name = "Michael Russo";
   fish = {
     enable = true;
 
+    plugins =
+      if pkgs.stdenv.isLinux
+      then
+        [
+          # Set up the Nix environment for a non-NixOS Fish shell. See:
+          # https://github.com/lilyball/nix-env.fish and
+          # https://www.reddit.com/r/Nix/comments/zyqv7z/fish_shell_not_setting_nix_paths/
+          # for more context.
+          {
+            name = "nix-env";
+            src = pkgs.fetchFromGitHub {
+              owner = "lilyball";
+              repo = "nix-env.fish";
+              rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+              sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+            };
+          }
+        ]
+      else
+        [];
+
     shellInitLast = ''
       # Store private environment variables (which aren't committed to this
       # repository) in ~/.localrc.fish.
