0f8f82dc91ff5263e800e1a11c49ab9479fe23b3
Michael Russo <mjr@mjrusso.com>
2025-01-11 07:30:17 -0500

    Ghostty config: don't quote values.
    (Just a minor nit, for consistency reasons; this change doesn't make any
    technical difference to how Ghostty handles the config.)

--- modules/shared/config/ghostty.nix
+++ modules/shared/config/ghostty.nix
@@ -2,16 +2,16 @@
   text = ''
     # Presentation
     theme = dark:Raycast_Dark,light:Raycast_Light
-    font-family = "Berkeley Mono"
+    font-family = Berkeley Mono
     font-size = 16
     cursor-style = block
     mouse-hide-while-typing = true
     window-width = 110
     window-height = 50
     window-padding-balance = true
-    window-colorspace = "display-p3"
-    macos-titlebar-style = "tabs"
-    macos-non-native-fullscreen = "visible-menu"
+    window-colorspace = display-p3
+    macos-titlebar-style = tabs
+    macos-non-native-fullscreen = visible-menu
 
     # Shell [NOTE: shell integration configured manually]
     shell-integration = none
