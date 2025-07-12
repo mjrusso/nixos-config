04909fbd7f5e37998849973272b4418497df9fe0
Michael Russo <mjr@mjrusso.com>
2025-07-11 21:13:58 -0400

    Remove unused `apply` scripts.

--- flake.nix
+++ flake.nix
@@ -52,13 +52,11 @@
         '')}/bin/${scriptName}";
       };
       mkLinuxApps = system: {
-        "apply" = mkApp "apply" system;
         "build" = mkApp "build" system;
         "build-switch" = mkApp "build-switch" system;
         "install" = mkApp "install" system;
       };
       mkDarwinApps = system: {
-        "apply" = mkApp "apply" system;
         "build" = mkApp "build" system;
         "build-switch" = mkApp "build-switch" system;
         "rollback" = mkApp "rollback" system;
