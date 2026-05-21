6c45851bfffef5d4a528039ea1465d652d524477
Michael Russo <mjr@mjrusso.com>
2026-05-21 19:45:17 -0400

    Add numtide binary cache
    See: https://github.com/numtide/llm-agents.nix#binary-cache

--- modules/shared/caches/default.nix
+++ modules/shared/caches/default.nix
@@ -4,11 +4,13 @@
     substituters = [
       "https://cache.garnix.io"
       "https://nix-community.cachix.org"
+      "https://cache.numtide.com"
       "https://cache.nixos.org/"
     ];
     trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
 }
